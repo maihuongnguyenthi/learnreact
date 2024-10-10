9f4f8d4414250bafd3ba8394eb4b54c9bfac4f8e
maihuongnguyenthi <huongnt.11.work@gmail.com>
2024-10-10 16:54:36 +0700

    two way bindings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import TwoWayBinding from './twoWayBinding'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <TwoWayBinding />
   </React.StrictMode>
 );
 
