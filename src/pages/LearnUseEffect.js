ccfd193a7cec79273b24dca8fa6858d06366e95c
maihuongnguyenthi <huongnt.11.work@gmail.com>
2024-10-11 00:16:00 +0700

    learn use effect with dependence

--- src/pages/LearnUseEffect.js
+++ src/pages/LearnUseEffect.js
@@ -6,25 +6,40 @@ import { useEffect, useState } from "react";
 //2. gọi lại 1 lần đầu thoi sau khi mounted vào dom
 //3. gọi lại khi giá trị phụ thuộc thay đổi
 
+const tabs = ['posts', 'comments', 'albums']
 
 const LearnUseEffect = () => {
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
+    const [item, setItem] = useState('posts')
 
     useEffect(() => {
         document.title = title
     })
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(`https://jsonplaceholder.typicode.com/${item}`)
             .then(res => res.json())
             .then(posts => {
                 setPosts(posts);
             })
-    }, [])
+    }, [item])
+
     return (
         <>
             <h1>learn-useEffect</h1>
+            <div>
+                {tabs.map(tab => (
+                    <button
+                        key={tab}
+                        style={item === tab ? { color: '#333', backgroundColor: '#fff' } : {}}
+                        onClick={() => setItem(tab)}
+                    >
+                        {tab}
+                    </button>
+                ))}
+            </div>
+
             <input
                 value={title}
                 onChange={e => setTitle(e.target.value)}
@@ -32,7 +47,7 @@ const LearnUseEffect = () => {
 
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
         </>
