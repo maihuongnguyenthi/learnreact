8533d048e62648fa5dacdf4d626afcfcea488ffd
maihuongnguyenthi <huongnt.11.work@gmail.com>
2024-10-12 13:41:53 +0700

    diferent useEffect anf useLayoutEffect

--- src/pages/LearnUseEffectChatApp.js
+++ src/pages/LearnUseEffectChatApp.js
@@ -1,5 +1,18 @@
 import { useEffect, useState } from "react";
-//
+//Phân biệt useEffect và useLayoutEffect
+//useEffect
+//1. Cập nhật lại state
+//2.Cập nhật DOM (mutated)
+//3.Render lại UI
+//4. Gọi cleanup nếu deps thay đổi
+//5. gọi useEffect callback()
+
+//useLayoutEffect
+//1. Cập nhật lại state
+//2.Cập nhật DOM (mutated)
+//3.Gọi cleanup nếu deps thay đổi(sync)
+//4. gọi useLayoutEffect callback(sync)
+//5.Render lại UI
 
 const lessons = [
     {
@@ -18,6 +31,7 @@ const lessons = [
 
 const LearnUseEffectChatApp = () => {
     const [lessonId, setLessonId] = useState(1)
+    const [count, setCount] = useState(0)
 
     useEffect(() => {
         const handleComment = ({ detail }) => {
@@ -26,6 +40,13 @@ const LearnUseEffectChatApp = () => {
         window.addEventListener(`lesson-${lessonId}`, handleComment)
         return () => window.removeEventListener(`lesson-${lessonId}`, handleComment)
     }, [lessonId])
+
+    //Chỗ này nên sử dụng useLayoutEffect()
+    useEffect(() => {
+        if (count > 3) {
+            setCount(0)
+        }
+    }, [count])
 
     return (
         <>
@@ -41,7 +62,8 @@ const LearnUseEffectChatApp = () => {
                     </li>
                 ))}
             </ul>
-
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Count</button>
         </>
     )
 };
