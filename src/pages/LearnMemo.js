f33ff0cd3ceb9f6d6a65a2668f8776151e5987af
maihuongnguyenthi <huongnt.11.work@gmail.com>
2024-10-12 22:09:03 +0700

    lean useCallback

--- src/pages/LearnMemo.js
+++ src/pages/LearnMemo.js
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import TestMemo from "./TestMemo";
-//Dùng để kiểm tra các component con có giá trị prop 
+//1.memo :Dùng để kiểm tra các component con có giá trị prop 
 //thay đổi không nếu không thay đổi không cần re-render lại
+//2. useCallback() thường đc dùng kết hợp với memo để ghi nhớ các prop là các hàm tham chiếu(function)
 
 const LearnMemo = () => {
     const [count, setCount] = useState(1)
     const [count2, setCount2] = useState(1)
 
+    const handleCount = useCallback(() => {
+        setCount(prev => prev + 1)
+    }, [])
     return <div>
         <h1>Learn memo</h1>
-        <TestMemo count={count} />
+        <TestMemo count={count} onCount={handleCount} />
         <h1>{count}</h1>
         <h1>{count2}</h1>
-        <button onClick={() => setCount(count + 1)}>Count 1</button>
+        {/* <button onClick={handleCount}>Count 1</button> */}
         <button onClick={() => setCount2(count2 + 1)}>Count 2</button>
     </div>;
 };
